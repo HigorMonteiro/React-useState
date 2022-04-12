e78bdc5912c55f94b6418eda1d2709dba27f9403
higorvinicius <higorvmonteiro@gmail.com>
2022-04-12 11:14:36 -0300

    React Hooks activate first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Modal from './Modal';
+import ButtonModal from './ButtonModal';
 
-function App() {
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState({
+    usernma: 'Higor',
+    age: '28',
+  });
+  const [modal, setModal] = React.useState(false);
+
+  function handleClick() {
+    setAtivo(!ativo);
+    setDados({ ...dados, job: 'FullStack developer' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="item">
+        <b>Username:</b> {dados.usernma}
+      </div>
+      <div className="item">
+        <b>Age:</b> {dados.age}
+      </div>
+
+      <div className="item">
+        <b>Job:</b>
+        {ativo ? dados.job : 'Inativo'}
+      </div>
+      <div className="item">
+        <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
+      </div>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
     </div>
   );
-}
+};
 
 export default App;
